2ea2e755e7f40c314a83569b13bf91cbf12bbbab
johnnylyimo <johnnylyimo@gmail.com>
2020-01-09 15:45:29 +0300

    Defined getCurrentLocation returned Future<void>

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -5,7 +5,7 @@ class Location {
   double latitude;
   double longitude;
 
-  void getCurrentLocation() async {
+  Future<void> getCurrentLocation() async {
     try {
       Position position = await Geolocator()
           .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
