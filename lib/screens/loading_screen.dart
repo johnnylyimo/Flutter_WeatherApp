60420660f198dd6fe5c9e0fa67e57801056f934c
johnnylyimo <johnnylyimo@gmail.com>
2020-01-11 18:10:39 +0300

    Imported LocationScreen package

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/screens/location_screen.dart';
 import 'package:weather_app/services/location.dart';
 import 'package:weather_app/services/networking.dart';
 
