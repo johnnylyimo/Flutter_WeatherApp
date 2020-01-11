c9b6c40f736ef6bbb532b46b5eb7bdfaa85d4ac9
johnnylyimo <johnnylyimo@gmail.com>
2020-01-11 17:10:06 +0300

    Added colorFilter property to the DecorationImage

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -14,6 +14,8 @@ class _LocationScreenState extends State<LocationScreen> {
           image: DecorationImage(
             image: AssetImage('images/location.jpg'),
             fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.white.withOpacity(0.8), BlendMode.dstATop),
           ),
         ),
       ),
