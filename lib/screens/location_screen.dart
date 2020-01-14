8d353dcd2b4eb1ca6c0eee1706ed47af4ea5bb64
johnnylyimo <johnnylyimo@gmail.com>
2020-01-14 16:13:04 +0300

    Assigned temperature to 0

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -26,7 +26,9 @@ class _LocationScreenState extends State<LocationScreen> {
   void updateUI(dynamic weatherData) {
     setState(() {
       // avoid error, when location turn off or api server down
-      if (weatherData == null) {}
+      if (weatherData == null) {
+        temperature = 0;
+      }
 
       double temp = weatherData['main']['temp'];
       temperature = temp.toInt();
