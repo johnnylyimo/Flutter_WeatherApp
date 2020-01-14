15df25e385dad706925c2492afab1a86788f1a9c
johnnylyimo <johnnylyimo@gmail.com>
2020-01-14 17:02:37 +0300

    Defined Container child to null

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -32,6 +32,7 @@ class _CityScreenState extends State<CityScreen> {
               ),
               Container(
                 padding: EdgeInsets.all(20.0),
+                child: null,
               )
             ],
           ),
