5eade7c6ec1ac3e4fc9684568b86e7e153838e6d
johnnylyimo <johnnylyimo@gmail.com>
2020-01-14 12:51:27 +0300

    Updated Text widget with degree symbol

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -67,7 +67,7 @@ class _LocationScreenState extends State<LocationScreen> {
                 padding: EdgeInsets.only(left: 15.0),
                 child: Row(
                   children: <Widget>[
-                    Text('$temperature'),
+                    Text('$temperatureº'),
                     Text('*'),
                   ],
                 ),
