0bfaa0d5602956c099df8526c0bcc235f83c5cdb
johnnylyimo <johnnylyimo@gmail.com>
2020-02-04 20:31:57 +0300

    Updated locationAccurancy to High

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -8,7 +8,7 @@ class Location {
   Future<void> getCurrentLocation() async {
     try {
       Position position = await Geolocator()
-          .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
       latitude = position.latitude;
       longitude = position.longitude;
     } catch (e) {}
